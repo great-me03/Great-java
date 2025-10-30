7b226b6d2cda4df6c7bae37ab5ae1a0cbdb72c46
HAJI LAPTOP g55 <khizerqadri68@gmail.com>
2025-10-30 19:57:45 +0500

    first

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+alert("Welcome to JS Land!  Happy Coding!");
+
+
+alert("Error! Please enter a valid password.");
+
+alert("Welcome to JS Land!"); br
+
+alert("Welcome to JS Land...\nHappy Coding!");
+
+console.log("Hello.... I can run JS through my web browser's console.");
+
+
+
+
+
+
+
+
